f46ab6aed89f61e6867a7d26e246fa7eaceb0e01
James Zhong <thebitspud@gmail.com>
2024-01-21 09:14:38 -0800

    User data

--- modules/client/src/routes/project-details/index.tsx
+++ modules/client/src/routes/project-details/index.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import { View } from 'reshaped'
 import { Text } from 'reshaped/bundle'
+import { useSnapshot } from 'valtio'
+import { userData } from '../../utils/userData'
+import { Navigate, useParams } from 'react-router-dom'
 
 export const ProjectDetailPage = () => {
-  const sections = ['Math 100', 'Math 101', 'Phys 117', 'Chem 123', 'Engl 110']
-  const rooms = ['Room 1', 'Room 2', 'Room 3']
+  let { projectId } = useParams()
+  const snapshot = useSnapshot(userData)
+  const project = snapshot.projects.find(project => project.uuid === projectId)
+
+  if (!project) {
+    return <Navigate to='/projects' />
+  }
+
+  const { sections, rooms } = project
 
   return (
     <View direction='row' justify='center' gap={6} paddingBlock={10}>
@@ -12,7 +22,7 @@ export const ProjectDetailPage = () => {
         <View direction='column' align='center'>
           <Text variant='featured-1'>Classes</Text>
           {sections.map(section => (
-            <View>{section}</View>
+            <View>{section.title}</View>
           ))}
         </View>
       </View.Item>
@@ -20,7 +30,7 @@ export const ProjectDetailPage = () => {
         <View direction='column' align='center'>
           <Text variant='featured-1'>Rooms</Text>
           {rooms.map(room => (
-            <View>{room}</View>
+            <View>{room.title}</View>
           ))}
         </View>
       </View.Item>
